f6a784b848e2128e92cf5bd7244f6b08ae57efaf
njmorales <njmorales@ucsd.edu>
2022-01-27 13:31:42 -0800

    Revert "correct compile and run commands added"
    This reverts commit c3a807d98b1e07519adc5f8b4d61ae8108269011.

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -6,16 +6,11 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 
-// to compile: javac -cp "lib/\*;." MarkdownParseTest.java
-// to run: java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore MarkdownParseTest
-
-
 public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
     }
-
 
     @Test
     public void testFile1() throws IOException {
