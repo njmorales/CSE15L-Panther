78304c201d0d0238d1e33a19e296104db912bf57
njmorales <njmorales@ucsd.edu>
2022-02-02 14:20:24 -0800

    run commands added

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -6,6 +6,9 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 
+
+// javac -cp "lib/\*;." MarkdownParseTest.java
+// java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore MarkdownParseTest
 public class MarkdownParseTest {
     @Test
     public void addition() {
